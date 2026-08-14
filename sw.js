3ad249833e2b92adae0f1973a0d16a1cd7b646d3
letzbug <129280428+letzbug@users.noreply.github.com>
2026-08-14 07:36:55 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='eldo-jukebox-demo-v1';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
+const CACHE='eldo-jukebox-demo-v2';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./assets/eldoradio-logo.png','./assets/eldoradio-jukebox.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match('./index.html')))));
